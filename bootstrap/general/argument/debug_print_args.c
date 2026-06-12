97e9c62b82e37dee0f9c7a59ea9da3cc7ae55592
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-06-12 22:14:10 +0000

    moving to debug print

--- bootstrap/general/argument/debug_print_args.c
+++ bootstrap/general/argument/debug_print_args.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <tracey/argument/args.h>
 
-void compile(const TraceyOptions *opts)
+void debug_print_args(const TraceyOptions *opts)
 {
     printf("Input  : %s\n", opts->input_file);
     printf("Output : %s\n", opts->output_file);
     printf("Wall   : %d\n", opts->warning_all);
     printf("Wextra : %d\n", opts->warning_extra);
+
 }
