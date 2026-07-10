c99ee08fcf2995f27c7108ea9e026048579985fa
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-07-10 01:02:50 +0000

    updating argument header use class
    Signed-off-by: Uoc Azizah <uoctamika.codeberg.org@gmail.com>

--- bootstrap/include/tracey/argument/argument.hxx
+++ bootstrap/include/tracey/argument/argument.hxx
@@ -1,6 +1,31 @@
-#pragma once
+#ifndef ARGUMENT_HPP
+#define ARGUMENT_HPP
 
-#include <tracey/struct/args_struct.hxx>
+class TraceyOptions
+{
+public:
+    TraceyOptions() noexcept
+        : p_input_file(nullptr),
+          p_output_file(nullptr),
+          warning_all(false),
+          warning_extra(false)
+    {
+    }
 
-int parse_args(int argc, char **argv, TraceyOptions *opts);
-void debug_print_args(const TraceyOptions *opts);
+    int parse_args(int argc, char **argv);
+
+    void debug_print_args() const;
+
+    const char *get_input_file() const noexcept  { return p_input_file; }
+    const char *get_output_file() const noexcept { return p_output_file; }
+    bool get_warning_all() const noexcept        { return warning_all; }
+    bool get_warning_extra() const noexcept      { return warning_extra; }
+
+private:
+    const char *p_input_file;
+    const char *p_output_file;
+    bool warning_all;
+    bool warning_extra;
+};
+
+#endif /* ARGUMENT_HXX */
--- bootstrap/include/tracey/struct/args_struct.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct TraceyOptions
-{
-    const char *input_file;
-    int warning_all;
-    int warning_extra;
-    const char *output_file;
-} TraceyOptions;
