edd24bfb46a492a419ebe11814bf3c65d531a098
Uoc Azizah <uoctamika.codeberg.org@gmail.com>
2026-07-17 22:59:03 +0000

    updating header file with class
    Signed-off-by: Uoc Azizah <uoctamika.codeberg.org@gmail.com>

--- bootstrap/include/tracey/file/file.hxx
+++ bootstrap/include/tracey/file/file.hxx
@@ -1,7 +1,38 @@
-#pragma once
+#ifndef TRACEY_FILE_HXX
+#define TRACEY_FILE_HXX
 
-#include <tracey/struct/file_struct.hxx>
+#include <cstdio>
+#include <string>
+#include <vector>
 
-int tracey_file_open(TraceyFile *file, const char *path);
-void tracey_file_close(TraceyFile *file);
-void tracey_debug_file_print(TraceyFile *file);
+namespace tracey::file
+{
+
+class TraceyFile {
+public:
+    TraceyFile() noexcept;
+    ~TraceyFile();
+
+    TraceyFile(const TraceyFile&) = delete;
+    TraceyFile& operator=(const TraceyFile&) = delete;
+    TraceyFile(TraceyFile&& other) noexcept;
+    TraceyFile& operator=(TraceyFile&& other) noexcept;
+
+    bool open(const char* path);
+
+    void close();
+
+    void debug_print() const;
+
+    [[nodiscard]] const std::string& path() const noexcept { return path_; }
+    [[nodiscard]] const std::vector<char>& buffer() const noexcept { return buffer_; }
+
+private:
+    std::FILE* handle_ = nullptr;
+    std::string path_;
+    std::vector<char> buffer_;
+};
+
+}
+
+#endif // TRACEY_FILE_HXX
