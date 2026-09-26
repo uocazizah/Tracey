23b5f5e4a230396a3eb95b7248261113bc5b3fb3
Uoc Azizah <uocazizah.maintainer@gmail.com>
2026-09-26 19:01:47 +0700

    refactor: remove old argument module files
    Signed-off-by: Uoc Azizah <uocazizah.maintainer@gmail.com>

--- bootstrap/general/argument/argument_parse.c
+++ bootstrap/general/argument/argument_parse.c
@@ -1,151 +1,197 @@
 #include <tracey/argument.h>
-#include <stdio.h>
+#include "argument_internal.h"
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-Arguments* arguments_parse(int argc, char** argv) {
-    if (argc <= 0 || argv == NULL) return NULL;
+#define DEFAULT_MAX_ARGS 10000
 
-    Arguments* args = calloc(1, sizeof(Arguments));
-    if (!args) return NULL;
+/* Forward declarations */
+static tracey_args_result_t parse_optimization(const char* arg, int* i, int argc, char** argv, tracey_opt_level_t* out);
+static tracey_args_result_t parse_option_with_arg(int* i, int argc, char** argv, const char** out);
 
-    // Alokasikan array dengan kapasitas maksimum argc
-    args->input_files = malloc((size_t)argc * sizeof(char*));
-    args->include_paths = malloc((size_t)argc * sizeof(char*));
-    args->defines = malloc((size_t)argc * sizeof(char*));
-    if (!args->input_files || !args->include_paths || !args->defines) {
-        arguments_free(args);
-        return NULL;
+tracey_args_result_t tracey_args_parse(tracey_args_t** out_args, int argc, char** argv, const tracey_args_config_t* config)
+{
+    if (!out_args || argc < 0 || !argv) {
+        return TRACEY_ARGS_ERROR_INVALID;
     }
 
-    // Inisialisasi nilai default
-    args->input_file_count = 0;
-    args->include_path_count = 0;
-    args->define_count = 0;
-    args->output_file = NULL;
-    args->optimization_level = 0;
-    args->warnings_as_errors = false;
-    args->debug_info = false;
-    args->verbose = false;
-    args->help = false;
-    args->version = false;
+    /* Default configuration */
+    tracey_args_config_t default_config;
+    default_config.program_name = "tracey";
+    default_config.version_string = "1.0.0";
+    default_config.description = "Tracey compiler";
+    default_config.max_args = DEFAULT_MAX_ARGS;
+    default_config.allow_unknown_options = false;
+    if (config) default_config = *config;
 
+    if (argc > (int)default_config.max_args) {
+        return TRACEY_ARGS_ERROR_TOO_MANY;
+    }
+
+    /* Allocate main structure */
+    tracey_args_t* args = calloc(1, sizeof(tracey_args_t));
+    if (!args) return TRACEY_ARGS_ERROR_NOMEM;
+
+    args->program_name = default_config.program_name;
+    args->version_string = default_config.version_string;
+    args->optimization = TRACEY_OPT_NONE;
+
+    /* Initialize dynamic arrays */
+    args->input_capacity = INITIAL_CAPACITY;
+    args->include_capacity = INITIAL_CAPACITY;
+    args->define_capacity = INITIAL_CAPACITY;
+
+    args->input_files = calloc(args->input_capacity, sizeof(char*));
+    args->include_paths = calloc(args->include_capacity, sizeof(char*));
+    args->defines = calloc(args->define_capacity, sizeof(char*));
+
+    if (!args->input_files || !args->include_paths || !args->defines) {
+        tracey_args_free(args);
+        return TRACEY_ARGS_ERROR_NOMEM;
+    }
+
+    /* Parse arguments */
     for (int i = 1; i < argc; i++) {
-        char* arg = argv[i];
+        const char* arg = argv[i];
+        if (!arg) continue;
 
         if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0) {
             args->help = true;
-        } else if (strcmp(arg, "-v") == 0 || strcmp(arg, "--verbose") == 0) {
-            args->verbose = true;
-        } else if (strcmp(arg, "--version") == 0) {
+            return TRACEY_ARGS_HELP_REQUESTED;
+        }
+        else if (strcmp(arg, "--version") == 0) {
             args->version = true;
-        } else if (strcmp(arg, "-Werror") == 0) {
+            return TRACEY_ARGS_VERSION_REQUESTED;
+        }
+        else if (strcmp(arg, "-v") == 0 || strcmp(arg, "--verbose") == 0) {
+            args->verbose = true;
+        }
+        else if (strcmp(arg, "-Werror") == 0) {
             args->warnings_as_errors = true;
-        } else if (strcmp(arg, "-g") == 0) {
+        }
+        else if (strcmp(arg, "-g") == 0) {
             args->debug_info = true;
-        } else if (strcmp(arg, "-o") == 0 || strcmp(arg, "--output") == 0) {
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Option '%s' requires an argument\n", arg);
-                arguments_free(args);
-                return NULL;
+        }
+        else if (strcmp(arg, "-o") == 0 || strcmp(arg, "--output") == 0) {
+            const char* out_file = NULL;
+            tracey_args_result_t res = parse_option_with_arg(&i, argc, argv, &out_file);
+            if (res != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return res;
             }
-            args->output_file = argv[++i];
-        } else if (strcmp(arg, "-I") == 0 || strcmp(arg, "--include") == 0) {
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Option '%s' requires an argument\n", arg);
-                arguments_free(args);
-                return NULL;
+            args->output_file = (char*)out_file;
+        }
+        else if (strcmp(arg, "-I") == 0 || strcmp(arg, "--include") == 0) {
+            const char* path = NULL;
+            tracey_args_result_t res = parse_option_with_arg(&i, argc, argv, &path);
+            if (res != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return res;
             }
-            args->include_paths[args->include_path_count++] = argv[++i];
-        } else if (strcmp(arg, "-D") == 0 || strcmp(arg, "--define") == 0) {
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Option '%s' requires an argument\n", arg);
-                arguments_free(args);
-                return NULL;
+            if (args_add_string(&args->include_paths, &args->include_count, &args->include_capacity, path) != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_NOMEM;
             }
-            args->defines[args->define_count++] = argv[++i];
-        } else if (strncmp(arg, "-O", 2) == 0) {
-            // Format: -O<level> atau -O <level>
-            const char* level_str = NULL;
-            if (arg[2] != '\0') {
-                level_str = arg + 2;
-            } else {
-                if (i + 1 >= argc) {
-                    fprintf(stderr, "Option '-O' requires an argument\n");
-                    arguments_free(args);
-                    return NULL;
+        }
+        else if (strcmp(arg, "-D") == 0 || strcmp(arg, "--define") == 0) {
+            const char* def = NULL;
+            tracey_args_result_t res = parse_option_with_arg(&i, argc, argv, &def);
+            if (res != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return res;
+            }
+            if (args_add_string(&args->defines, &args->define_count, &args->define_capacity, def) != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_NOMEM;
+            }
+        }
+        else if (strncmp(arg, "-O", 2) == 0) {
+            tracey_args_result_t res = parse_optimization(arg, &i, argc, argv, &args->optimization);
+            if (res != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return res;
+            }
+        }
+        else if (strncmp(arg, "--optimize", 10) == 0) {
+            if (strcmp(arg, "--optimize") == 0) {
+                const char* level = NULL;
+                tracey_args_result_t res = parse_option_with_arg(&i, argc, argv, &level);
+                if (res != TRACEY_ARGS_OK) {
+                    tracey_args_free(args);
+                    return res;
                 }
-                level_str = argv[++i];
+                if (strlen(level) != 1 || level[0] < '0' || level[0] > '3') {
+                    tracey_args_free(args);
+                    return TRACEY_ARGS_ERROR_INVALID;
+                }
+                args->optimization = (tracey_opt_level_t)(level[0] - '0');
             }
-            if (strlen(level_str) != 1 || level_str[0] < '0' || level_str[0] > '3') {
-                fprintf(stderr, "Invalid optimization level: '%s' (expected 0-3)\n", level_str);
-                arguments_free(args);
-                return NULL;
+            else if (strncmp(arg, "--optimize=", 11) == 0) {
+                const char* level = arg + 11;
+                if (strlen(level) != 1 || level[0] < '0' || level[0] > '3') {
+                    tracey_args_free(args);
+                    return TRACEY_ARGS_ERROR_INVALID;
+                }
+                args->optimization = (tracey_opt_level_t)(level[0] - '0');
             }
-            args->optimization_level = level_str[0] - '0';
-        } else if (strncmp(arg, "--optimize=", 11) == 0) {
-            const char* level_str = arg + 11;
-            if (strlen(level_str) != 1 || level_str[0] < '0' || level_str[0] > '3') {
-                fprintf(stderr, "Invalid optimization level: '%s' (expected 0-3)\n", level_str);
-                arguments_free(args);
-                return NULL;
+            else if (default_config.allow_unknown_options) {
+                /* Unknown --optimize* variant */
             }
-            args->optimization_level = level_str[0] - '0';
-        } else if (strcmp(arg, "--optimize") == 0) {
-            if (i + 1 >= argc) {
-                fprintf(stderr, "Option '--optimize' requires an argument\n");
-                arguments_free(args);
-                return NULL;
+            else {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_INVALID;
             }
-            const char* level_str = argv[++i];
-            if (strlen(level_str) != 1 || level_str[0] < '0' || level_str[0] > '3') {
-                fprintf(stderr, "Invalid optimization level: '%s' (expected 0-3)\n", level_str);
-                arguments_free(args);
-                return NULL;
+        }
+        else if (arg[0] == '-') {
+            if (default_config.allow_unknown_options) {
+                /* Silently ignore unknown options if configured */
             }
-            args->optimization_level = level_str[0] - '0';
-        } else if (arg[0] == '-') {
-            fprintf(stderr, "Unknown option: '%s'\n", arg);
-            arguments_free(args);
-            return NULL;
-        } else {
-            // Input file
-            args->input_files[args->input_file_count++] = arg;
+            else {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_INVALID;
+            }
+        }
+        else {
+            /* Positional argument - input file */
+            if (arg[0] == '\0') {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_EMPTY_ARG;
+            }
+            if (args_add_string(&args->input_files, &args->input_count, &args->input_capacity, arg) != TRACEY_ARGS_OK) {
+                tracey_args_free(args);
+                return TRACEY_ARGS_ERROR_NOMEM;
+            }
         }
     }
 
-    // Kecilkan array agar sesuai dengan jumlah elemen aktual
-    if (args->input_file_count > 0) {
-        char** tmp = realloc(args->input_files, args->input_file_count * sizeof(char*));
-        if (tmp) args->input_files = tmp;
-    } else {
-        free(args->input_files);
-        args->input_files = NULL;
-    }
-
-    if (args->include_path_count > 0) {
-        char** tmp = realloc(args->include_paths, args->include_path_count * sizeof(char*));
-        if (tmp) args->include_paths = tmp;
-    } else {
-        free(args->include_paths);
-        args->include_paths = NULL;
-    }
-
-    if (args->define_count > 0) {
-        char** tmp = realloc(args->defines, args->define_count * sizeof(char*));
-        if (tmp) args->defines = tmp;
-    } else {
-        free(args->defines);
-        args->defines = NULL;
-    }
-
-    return args;
+    *out_args = args;
+    return TRACEY_ARGS_OK;
 }
 
-void arguments_free(Arguments* args) {
-    if (!args) return;
-    free(args->input_files);
-    free(args->include_paths);
-    free(args->defines);
-    free(args);
+/* Parse -O<level> or -O <level> */
+static tracey_args_result_t parse_optimization(const char* arg, int* i, int argc, char** argv, tracey_opt_level_t* out)
+{
+    const char* level_str = NULL;
+    if (arg[2] != '\0') {
+        level_str = arg + 2;
+    } else {
+        if (*i + 1 >= argc) return TRACEY_ARGS_ERROR_MISSING_ARG;
+        level_str = argv[++(*i)];
+    }
+    if (strlen(level_str) != 1 || level_str[0] < '0' || level_str[0] > '3') {
+        return TRACEY_ARGS_ERROR_INVALID;
+    }
+    *out = (tracey_opt_level_t)(level_str[0] - '0');
+    return TRACEY_ARGS_OK;
+}
+
+/* Parse option that requires an argument (e.g., -o file, -I path) */
+static tracey_args_result_t parse_option_with_arg(int* i, int argc, char** argv, const char** out)
+{
+    if (*i + 1 >= argc) return TRACEY_ARGS_ERROR_MISSING_ARG;
+    const char* val = argv[++(*i)];
+    if (val[0] == '\0') return TRACEY_ARGS_ERROR_EMPTY_ARG;
+    *out = val;
+    return TRACEY_ARGS_OK;
 }
--- bootstrap/include/tracey/argument.h
+++ bootstrap/include/tracey/argument.h
@@ -1,43 +1,84 @@
-#ifndef ARGUMENT_H
-#define ARGUMENT_H
+#ifndef TRACEY_ARGUMENT_H
+#define TRACEY_ARGUMENT_H
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Opaque handle for argument parser state */
+typedef struct tracey_args tracey_args_t;
+
+/* Parse result codes */
+typedef enum {
+    TRACEY_ARGS_OK = 0,
+    TRACEY_ARGS_ERROR_NOMEM = -1,
+    TRACEY_ARGS_ERROR_INVALID = -2,
+    TRACEY_ARGS_ERROR_TOO_MANY = -3,
+    TRACEY_ARGS_ERROR_MISSING_ARG = -4,
+    TRACEY_ARGS_ERROR_EMPTY_ARG = -5,
+    TRACEY_ARGS_HELP_REQUESTED = 1,
+    TRACEY_ARGS_VERSION_REQUESTED = 2,
+} tracey_args_result_t;
+
+/* Optimization levels */
+typedef enum {
+    TRACEY_OPT_NONE = 0,
+    TRACEY_OPT_LEVEL1 = 1,
+    TRACEY_OPT_LEVEL2 = 2,
+    TRACEY_OPT_LEVEL3 = 3,
+} tracey_opt_level_t;
+
+/* Argument parser configuration */
 typedef struct {
-    char** input_files;
-    size_t input_file_count;
-    char* output_file;
-    char** include_paths;
-    size_t include_path_count;
-    char** defines;
-    size_t define_count;
-    int optimization_level;
-    bool warnings_as_errors;
-    bool debug_info;
-    bool verbose;
-    bool help;
-    bool version;
-    char _padding[7];
-} Arguments;
+    const char* program_name;
+    const char* version_string;
+    const char* description;
+    size_t max_args;
+    bool allow_unknown_options;
+} tracey_args_config_t;
 
-Arguments* arguments_parse(int argc, char** argv);
-void arguments_free(Arguments* args);
+/* Parse command line arguments */
+tracey_args_result_t tracey_args_parse(tracey_args_t** out_args,
+                                       int argc, char** argv,
+                                       const tracey_args_config_t* config);
 
-bool arguments_has_input_files(const Arguments* args);
-const char* arguments_get_output_file(const Arguments* args);
-int arguments_get_optimization_level(const Arguments* args);
-bool arguments_get_warnings_as_errors(const Arguments* args);
-bool arguments_get_debug_info(const Arguments* args);
-bool arguments_get_verbose(const Arguments* args);
-bool arguments_get_help(const Arguments* args);
-bool arguments_get_version(const Arguments* args);
+/* Free parsed arguments */
+void tracey_args_free(tracey_args_t* args);
 
-const char* arguments_find_define(const Arguments* args, const char* name);
-bool arguments_has_include_path(const Arguments* args, const char* path);
+/* Accessor functions */
+bool tracey_args_has_help(const tracey_args_t* args);
+bool tracey_args_has_version(const tracey_args_t* args);
+bool tracey_args_has_verbose(const tracey_args_t* args);
+bool tracey_args_has_warnings_as_errors(const tracey_args_t* args);
+bool tracey_args_has_debug_info(const tracey_args_t* args);
 
-void arguments_print_help(const char* program_name);
-void arguments_print_version(const char* program_name, const char* version_string);
-void arguments_print_debug(const Arguments* args);
+tracey_opt_level_t tracey_args_get_optimization(const tracey_args_t* args);
+const char* tracey_args_get_output(const tracey_args_t* args);
+size_t tracey_args_input_count(const tracey_args_t* args);
+const char* tracey_args_get_input(const tracey_args_t* args, size_t index);
+size_t tracey_args_include_count(const tracey_args_t* args);
+const char* tracey_args_get_include(const tracey_args_t* args, size_t index);
+size_t tracey_args_define_count(const tracey_args_t* args);
+const char* tracey_args_get_define(const tracey_args_t* args, size_t index);
+const char* tracey_args_find_define(const tracey_args_t* args, const char* name);
+bool tracey_args_has_include_path(const tracey_args_t* args, const char* path);
 
-#endif /* ARGUMENT_H */
+/* Help and version printing */
+void tracey_args_print_help(const tracey_args_config_t* config, FILE* out);
+void tracey_args_print_version(const tracey_args_config_t* config, FILE* out);
+
+/* Debug output */
+void tracey_args_print_debug(const tracey_args_t* args, FILE* out);
+
+/* Error string for result codes */
+const char* tracey_args_strerror(tracey_args_result_t result);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TRACEY_ARGUMENT_H */
